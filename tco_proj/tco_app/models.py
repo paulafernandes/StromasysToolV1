7a635215a9e6b80224e53a4e6ddba106f96db09f
Paula Fernandes <paula.r.fernandes@gmail.com>
2018-11-30 17:41:10 +0100

    Database created

--- tco_proj/tco_app/models.py
+++ tco_proj/tco_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class SystemTable(models.Model):
+    system_name = models.CharField(max_length=50)
+
+class ModelTable(models.Model):
+    model_name = models.CharField(max_length=50)
+    power_name = models.CharField(max_length=50)
+    power_watt = models.IntegerField(default=0)    
+    id_system = models.ForeignKey(SystemTable, on_delete=models.CASCADE)
+
+class LicenceTable(models.Model):
+    licence_name = models.CharField(max_length=50)
+    licence_cost = models.IntegerField(default=0)
+
+class CpuTable(models.Model):
+    min_cpu = models.IntegerField(default=0)
+    max_cpu = models.IntegerField(default=0)
+    id_model = models.ForeignKey(ModelTable, on_delete=models.CASCADE)
+    id_licence = models.ForeignKey(LicenceTable, on_delete=models.CASCADE)
--- tco_proj/tco_proj/settings.py
+++ tco_proj/tco_proj/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'tco_app.apps.TcoAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
