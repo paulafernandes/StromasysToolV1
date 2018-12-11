1ac1cae5ae0b419a84d1f585048d57022c7f771b
Paula Fernandes <paula.r.fernandes@gmail.com>
2018-12-11 14:43:42 +0100

    Ajax call working for 2 dropdowns

--- tco_proj/tco_app/urls.py
+++ tco_proj/tco_app/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
     path('teste_page', views.modelsNames, name='teste-page'),
     # path('filter_model', system_select, name='filter-models-page'),
     # path('tco_app/<int:pk>/all_json_models/', views.all_json_models),
-    path('simulation_page', views.system_choice ,name='sims-page'),
+    path('simulation_page', views.system_choice, name='sims-page'),
     # path('tco_app/about.html', views.index, name='index'),
-    path('all_json_models/', views.all_json_models, name='all_json_models'),
+    # (r’^brand/(?P<brand>[-\w]+)/all_json_models/$’, ‘all_json_models’),
+    path('simulation_page/<int:pk>/all_json_models/', views.all_json_models, name='all_json_models'),
 ]
--- tco_proj/tco_app/views.py
+++ tco_proj/tco_app/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from .models import SystemTable, ModelTable, LicenceTable, CpuTable
 from django.core import serializers
 import json
+import sys
+# from django.views.decorators.csrf import ensure_csrf_cookie
 
 def index(request):
     context = {
@@ -20,28 +22,51 @@ def modelsNames(request):
     return render(request, "tco_app/teste.html", context)
 
 def system_choice(request):
-    return render(request, "tco_app/simulation.html")
+    context = {
+        'systems': SystemTable.objects.all()
+    }
+    return render(request, "tco_app/simulation.html", context)
 
-def all_json_models(request, system):
-    if request.method == 'POST':
-        post_text = request.POST.get('id_system')
-        response_data = {}
+def all_json_models(request, pk):
+    # id_system = ModelTable.objects.get(id_system=systemid)
+    models = ModelTable.objects.all().filter(id_system=pk)
+    # sys.stderr.write('*********************: ' + 
+    # str(ModelTable.objects.all().filter(id_system=systemid).count()))
+    json_models = serializers.serialize('json', models)
+    return HttpResponse(json_models, content_type='application/javascript')
 
-        # post = Post(text=post_text, author=request.user)
-        # post.save()
+# # @ensure_csrf_cookie
+# # def all_json_models(request):
+# def all_json_models(request, idsystem):
+#     sys.stderr.write('teste')
+#     if request.method == 'GET':
+#         systemid = request.POST.get('id_system')
+#         response_data = {}
 
-        response_data['result'] = 'Create post successful!'
-        response_data['postpk'] = post_text
-        # response_data['text'] = post.text
-        # response_data['created'] = post.created.strftime('%B %d, %Y %I:%M %p')
-        # response_data['author'] = post.author.username
+#         models = ModelTable.objects.filter(id_system=idsystem)
+#         # post = Post(text=post_text, author=request.user)
+#         # post.save()
 
-        return HttpResponse(
-            json.dumps(response_data),
-            content_type="application/json"
-        )
-    else:
-        return HttpResponse(
-            json.dumps({"nothing to see": "this isn't happening"}),
-            content_type="application/json"
-        )
+#         response_data['result'] = models
+#         # response_data['postpk'] = post_text
+#         # response_data['text'] = post.text
+#         # response_data['created'] = post.created.strftime('%B %d, %Y %I:%M %p')
+#         # response_data['author'] = post.author.username
+
+#         return HttpResponse(
+#             json.dumps(response_data),
+#             content_type="application/json"
+#         )
+#         # return HttpResponse(
+#         #     json.dumps(response_data),
+#         #     content_type="application/json"
+#         # )
+#     else:
+#         return HttpResponse(
+#             json.dumps({"nothing to see": "this isn't happening"}),
+#             content_type="application/json"
+#         )
+#         # return HttpResponse(
+#         #     json.dumps({"nothing to see": "this isn't happening"}),
+#         #     content_type="application/json"
+#         # )
