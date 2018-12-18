acfdd55283afb31df3cb85cd83ce4322eb5b65f6
Paula Fernandes <paula.r.fernandes@gmail.com>
2018-12-18 13:54:11 +0100

    Form working & validating. All output working.

--- tco_proj/tco_app/static/tco_app/scripts.js
+++ tco_proj/tco_app/static/tco_app/scripts.js
@@ -2,6 +2,11 @@ document.addEventListener('DOMContentLoaded', () => {
     $("select#allsystems").on('change', function() {
         if ($(this).val() == 0) {
             $("#divModels").css('display', 'none');
+            $("#divCpus").css('display', 'none');
+            $("#lblCpu").css('display', 'none');
+            $("#divPay").css('display', 'none');
+            $("#btnSim").css('display', 'none');
+            $("#divCurrency").css('display', 'none');
 		}
         else {
             var url = "/simulation_page/" + $(this).val() + "/all_json_models/";
@@ -16,22 +21,57 @@ document.addEventListener('DOMContentLoaded', () => {
                 $("select#allmodels option:first").attr('selected', 'selected');
 			});
 		}
-    });
-
+	});
+	
+    (function() {
+        'use strict';
+        window.addEventListener('load', function() {
+			// Fetch all the forms we want to apply custom Bootstrap validation styles to
+			var forms = document.getElementsByClassName('needs-validation');
+			// Loop over them and prevent submission
+			var validation = Array.prototype.filter.call(forms, function(form) {
+				form.addEventListener('submit', function(event) {
+					if (form.checkValidity() === false) {
+						event.preventDefault();
+						event.stopPropagation();
+					}
+					form.classList.add('was-validated');
+				}, false);
+			});
+		}, false);
+	})();
+	
     $("#frmSim").on('submit',  function(event) {
         event.preventDefault();
-        var idcpu;
-        if ($('#cpulabel').css('display') == 'block')
+        pay = $('#txtPay').val();
+        coin = $("select#allcurrency option:selected").val();
+        cpus = $("select#allcpus option:selected").val();
+        if (pay != "" && coin != "" && cpus != "") {        
+			var idcpu;
+			if ($('#cpulabel').css('display') == 'block')
             idcpu = $('#cpulabel').attr('cpuid');
-        else
+			else
             idcpu = $( "#allcpus option:selected" ).val();
-        payperyear = $('#txtPay').val();
-        currency = $( "#allcurrency option:selected" ).val();
-        var url = "/simulation_page/" + idcpu + "/" + payperyear + "/" + currency + "/json_simulation/";
-        $.getJSON(url, function(savings) {
-            console.log(JSON.stringify(savings));
-        });
-    });
+			payperyear = $('#txtPay').val();
+			currency = $( "#allcurrency option:selected" ).val();
+			var url = "/simulation_page/" + idcpu + "/" + payperyear + "/" + currency + "/json_simulation/";
+			$.getJSON(url, function(savings) {
+				console.log(JSON.stringify(savings));
+				$("#divSearch").css('display', 'none');
+				$("#divResults").css('display', 'block');
+				$("#lblf_total_savings_currency").html(savings.f_total_savings_currency);
+				$("#lblreduction_of").html(savings.reduction_of);
+				$("#lbli_power_savings").html(savings.i_power_savings);
+				$("#lbli_carbon_footprint_legacy").html(savings.i_carbon_footprint_legacy);
+				$("#lbli_carbon_footprint_savings").html(savings.i_carbon_footprint_savings);
+			})
+			// **************************************
+			// error handling
+			// .done(function() { alert('getJSON request succeeded!'); })
+			// .fail(function(jqXHR, textStatus, errorThrown) { alert('getJSON request failed! ' + textStatus); })
+			// .always(function() { alert('getJSON request ended!'); });
+		}
+	});
     
     $("select#allmodels").on('change', function() {
         if ($(this).val() == 0) {
@@ -45,7 +85,7 @@ document.addEventListener('DOMContentLoaded', () => {
             var url = "/simulation_page/" + $(this).val() + "/all_json_cpus/";
             $.getJSON(url, function(cpus) {
                 // console.log(JSON.stringify(cpus));
-                var options = '<option selected value="0">Select CPU</option>';
+                var options = '<option selected value="">Select CPU</option>';
                 if (cpus.length == 1)
                 {
                     $("#cpulabel").val(cpus[0].fields["min_cpu"]);
@@ -68,7 +108,7 @@ document.addEventListener('DOMContentLoaded', () => {
                         $("#divCpus").css('display', 'block');
                         // $("#divPay").css('display', 'block');
 					}        
-                }
+				}
                 $("#divPay").css('display', 'block');
                 $("#btnSim").css('display', 'block');
                 $("#divCurrency").css('display', 'block');
--- tco_proj/tco_app/urls.py
+++ tco_proj/tco_app/urls.py
@@ -2,16 +2,11 @@ from django.urls import path
 from django.conf.urls import url
 from . import views
 from .views import all_json_models
-# from .templatetags import system_select
 
 urlpatterns = [
     path('', views.index, name='index-page'),
     path('teste_page', views.modelsNames, name='teste-page'),
-    # path('filter_model', system_select, name='filter-models-page'),
-    # path('tco_app/<int:pk>/all_json_models/', views.all_json_models),
     path('simulation_page', views.system_choice, name='sims-page'),
-    # path('tco_app/about.html', views.index, name='index'),
-    # (r’^brand/(?P<brand>[-\w]+)/all_json_models/$’, ‘all_json_models’),
     path('simulation_page/<int:pk>/all_json_models/', views.all_json_models, name='all_json_models'),
     path('simulation_page/<int:pk>/all_json_cpus/', views.all_json_cpus, name='all_json_cpus'),
     path('simulation_page/<int:cpu>/<int:f_maintenance>/<str:currency>/json_simulation/', views.json_simulation, name='json_simulation'),
